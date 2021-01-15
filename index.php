09c3f097d4a54d39a1725da2cb67c0d441c31de5
Husrav Khomidov <kkhomidov2@mail.greenriver.edu>
2021-01-15 15:18:03 -0500

    Created and added index.php for PP2

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Husrav Khomidov
+ * Ben Chadwick
+ * PP2
+ * 1/15/2021
+ *
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pair Program 2</title>
+</head>
+<body>
+    <h1>Pair Program 2</h1>
+</body>
+</html>
+<?php
+
+
+echo "PHP Array Practice (Step 1)";
